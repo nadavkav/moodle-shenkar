6ade0e5df9df5f0276329f1d591ee05e9178dea4
Nadav Kavalerchik <moodle-dev@shenkar.ac.il>
2016-11-24 15:56:36 +0200

    adding block searchcourses

---
+++ blocks/searchcourses/lang/he/block_searchcourses.php
@@ -0,0 +1,6 @@
+<?php
+$string['pluginname'] = 'חיפוש קורסים (מהיר)';
+$string['course_count_label'] = 'מספר הקורסים להצגה';
+$string['search_label'] = 'חיפוש:';
+$string['searchcourses:addinstance'] = 'Add Search Courses block';
+$string['searchcourses:myaddinstance'] = 'Add Search Courses block to My Home';
