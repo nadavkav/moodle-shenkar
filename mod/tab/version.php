578912469d9a7415eb99af5ad1b56419e38d84da
Nadav Kavalerchik <moodle-dev@shenkar.ac.il>
2017-09-11 14:26:36 +0300

    mod_tab

---
+++ mod/tab/version.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * *************************************************************************
+ * *                         OOHOO - Tab Display                          **
+ * *************************************************************************
+ * @package     mod                                                       **
+ * @subpackage  tab                                                       **
+ * @name        tab                                                       **
+ * @copyright   oohoo.biz                                                 **
+ * @link        http://oohoo.biz                                          **
+ * @author      Patrick Thibaudeau                                        **
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later  **
+ * *************************************************************************
+ * ************************************************************************ */
+$plugin->version = 2015122900;  // The current module version (Date: YYYYMMDDXX)
+$plugin->requires = 2010112400;
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '2.2.1 (Build: 2015122900)';
+$plugin->cron = 0;           // Period for cron to check this module (secs)
+$plugin->component = 'mod_tab';
